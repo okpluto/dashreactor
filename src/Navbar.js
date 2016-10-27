dddb56c7030326923a3112d47e35c2e3b6e51613
Cameron Baughn <cam.baughn@gmail.com>
2016-10-27 10:40:06 -0700

    Handing off

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 'lessons'
+    }
+  }
 
 
   render() {
-    const { navbarStyle, brandStyle, menuItemStyle } = styles;
+    const { navbarStyle, brandStyle, menuItemStyle, selectedStyle } = styles;
 
     return (
-      <div className="Navbar col-md-4" style={navbarStyle}>
+      <div className="Navbar col-md-1" style={navbarStyle}>
         <h2 className="brand" style={brandStyle}>Dash Reactor</h2>
-        <i className="fa fa-book fa-2x" aria-hidden="true"></i>
-        <i className="fa fa-user fa-2x" aria-hidden="true"></i>
-        <h4 style={menuItemStyle}>Lessons</h4>
-        <h4 style={menuItemStyle}>Users</h4>
+        <i className="fa fa-book fa-2x" aria-hidden="true" style={{...menuItemStyle, ...selectedStyle}}></i>
+        <br />
+        <i className="fa fa-user fa-2x" aria-hidden="true" style={menuItemStyle}></i>
       </div>
     );
   }
@@ -25,9 +29,9 @@ const darkGrey = '#777A7D'
 const styles = {
   navbarStyle: {
     height: '100%',
-    backgroundColor: 'transparent',
+    backgroundColor: '#2c3e50',
     position: 'fixed',
-    paddingLeft: 70,
+    paddingLeft: 20,
     paddingTop: 100,
     fontFamily: 'Lato',
   },
@@ -38,7 +42,11 @@ const styles = {
     color: lightGrey,
     marginTop: 25,
     cursor: 'pointer',
-
+    opacity: 0.4,
+  },
+  selectedStyle: {
+    color: 'white',
+    opacity: 1,
   }
 }
 
