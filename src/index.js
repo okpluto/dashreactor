8eee89bcaaf41e59b0e3c8ede00d61aefd66bd78
annasophie <annasophie.nagel@gmail.com>
2016-11-01 02:09:23 -0400

    Initial update to readme Removed/Updated some comments Deleted unnecessary CSS files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
-// import './index.css';
-
-// import LessonTitleList from './LessonTitleCardList';
-// import Navbar from './Navbar';
-
-
-// ReactDOM.render((
-//   <Router history={browserHistory}>
-//     <Route path='/' component={App}>
-//       <Route component={Navbar} />
-//       {/*<Route path="nav" component={Navbar} />*/}
-//       <Route component={LessonTitleList} />
-//     </Route>
-//   </Router>
-//   ), document.getElementById('root')
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+// import './index.css';
 
 ReactDOM.render(
   <App />,
