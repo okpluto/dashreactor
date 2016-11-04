a7be2ac6a44a5d7d439d820f060830176a961e1d
katgurdak <katgurdak@gmail.com>
2016-11-03 21:03:28 -0700

    Made LessonServices
    - getLessons
    - getLessonById
    - Included jQuery cdn within index.html
    - Added bluebird to package.json
    - Made App file a .jsx for syntax highlighting

---
+++ services/LessonServices.js
@@ -0,0 +1,29 @@
+var Promise = require('bluebird');
+
+
+const getLessons = function () {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: 'http://localhost:3011/api/lessons/',
+      type: 'GET',
+      success: resolve,
+      error: reject
+    });
+  });
+};
+
+const getLessonById = function (lessonId) {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `http://localhost:3011/api/lessons/${lessonId}`,
+      type: 'GET',
+      success: resolve,
+      error: reject
+    });
+  });
+};
+
+module.exports = {
+  getLessons: getLessons,
+  getLessonById: getLessonById
+};
