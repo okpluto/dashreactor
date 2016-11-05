f53f60a255932f834bfec44a230db697b70db41d
Ivey Topaz <iveytopaz@gmail.com>
2016-11-05 17:02:04 -0400

    adding signin screen

---
+++ src/Signin.jsx
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import { Button, Col } from 'react-bootstrap'
+
+
+class SignInForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleSignInClick = this.handleSignInClick.bind(this)
+  }
+
+
+  handleSignInClick() {
+    let self = this
+    let url=`http://localhost:3011/api/users/signin`;
+    const data = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    const json = JSON.stringify(data);
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: json
+    })
+    .then(response => {
+      if (response.status === 404) {
+        return this.setState({
+          errorMessage: 'User does not exist',
+          username: '',
+          password:''
+        });
+      } else if (response.status === 401){
+        return this.setState({
+          errorMessage: 'Username or password do not match',
+          username: '',
+          password:''
+        });
+      } else {
+        return response.json();
+      }
+    })
+    .then(res => {
+      if (res) {
+        localStorage.setItem('userAuth', JSON.stringify({'jwt': res.token, 'id': res.id}))
+        self.props.checkLogin()
+      }
+    })
+  }
+
+  render() {
+    const { signinStyle, cardStyle, textStyle, darkTextStyle, lightTextStyle, textInputStyle } = styles;
+    return (
+      <Col sm={6} style={signinStyle}>
+        <span style={darkTextStyle}>Welcome to Scripty's Lesson Creator</span>
+        <div>
+          <input
+            style={textInputStyle}
+            placeholder={"username"}
+            value={this.state.username}
+            onChange={ (event) => this.setState({username: event.target.value})}
+          /><br />
+          <input
+            type={"password"}
+            style={textInputStyle}
+            value={this.state.password}
+            placeholder={"password"}
+            onChange={(event) => this.setState({password: event.target.value})}
+          />
+        </div>
+        <Button onClick={this.handleSignInClick} style={cardStyle} >
+          <span style={lightTextStyle}>Sign In</span>
+        </Button><br />
+        <span style={darkTextStyle}>{this.state.errorMessage}</span>
+      </Col>
+    )
+  }
+}
+
+
+
+const coral = '#FA848A'
+const darkCoral = '#DE757A'
+const grey = '#FAFAFA'
+
+const styles = {
+  signinStyle: {
+    height: '100%',
+    fontSize: 20,
+    backgroundColor: 'white',
+    position: 'fixed',
+    paddingTop: 60,
+    paddingRight: 0,
+    paddingLeft:15,
+    marginLeft: 100,
+    fontFamily: 'Lato',
+    zIndex: 0,
+    boxShadow: '-2px 0px 5px -2px rgba(0,0,0,0.2)',
+  },
+
+  cardStyle: {
+    height: 50,
+    marginTop: 20,
+    borderRadius: 5,
+    position: 'relative',
+    backgroundColor: coral,
+  },
+  darkTextStyle: {
+    color: coral,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  lightTextStyle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  textInputStyle: {
+    height: 50,
+    backgroundColor: 'white',
+    marginTop: 20,
+    borderRadius: 5,
+    borderColor: coral,
+    borderWidth: 1,
+    color: 'grey',
+    fontSize: 20,
+    textAlign: 'center'
+
+  }
+}
+
+
+export default SignInForm;
