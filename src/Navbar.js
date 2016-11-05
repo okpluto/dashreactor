562933e6e71e401ad271df7f538202fc7c2daafd
Ivey Topaz <iveytopaz@gmail.com>
2016-11-05 17:06:04 -0400

    changing nav bar for logged in and sign up views

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,23 +6,40 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'lessons'
+      selected: this.props.display
     }
+    this.logout = this.logout.bind(this)
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({selected: nextProps.display})
+  }
+
+  logout() {
+    localStorage.removeItem('userAuth')
+    this.props.checkLogin()
+  }
 
   render() {
-    const { navbarStyle, menuItemStyle, selectedStyle } = styles;
+    const { navbarStyle, menuItemStyle, selectedStyle, textStyle } = styles;
+    const getStyle = (icon, selected) => {
+      if (icon === selected) {
+        return {...menuItemStyle, ...selectedStyle}
+      } else {
+        return menuItemStyle
+      }
+    }
 
     return (
       <Col className="Navbar" style={navbarStyle}>
        {/* <h2 className="brand" style={brandStyle}>Dash Reactor</h2>*/}
         <img alt="dashReactorLogo" src={require('../public/dashReactorLogo.png')} style={{marginLeft: 10, marginTop: -90}}/>
-        <i className="fa fa-book fa-2x" aria-hidden="true" style={{...menuItemStyle, ...selectedStyle}}></i>
+        <i className="fa fa-book fa-2x" aria-hidden="true" style={getStyle('lessons', this.state.selected)}></i>
         <br />
 
       {/*this 'users' option is not currently implemented - app is hard coded to open up lessons only.*/}
-        <i className="fa fa-user fa-2x" aria-hidden="true" style={menuItemStyle}></i>
+        <i className="fa fa-user fa-2x" aria-hidden="true" style={getStyle('user', this.state.selected)} onClick={this.logout}></i>
+        <span style={textStyle} onClick={this.logout}>{(this.state.selected === 'lessons' ? 'Logout' : '')}</span>
       </Col>
     );
   }
@@ -56,6 +73,11 @@ const styles = {
   selectedStyle: {
     color: coral,
     opacity: 1,
+  },
+
+  textStyle : {
+    color: lightGrey,
+    paddingLeft: 18
   }
 }
 
